808da73ffcdd4148916f4af41ceca2429daad6da
Robert Enzmann <robert.enzmann@antuit.com>
2021-09-12 20:03:43 -0400

    update python script

--- python/copy_using_pandas.py
+++ python/copy_using_pandas.py
@@ -5,11 +5,11 @@ import sqlalchemy as sa
 
 
 if __name__ == '__main__':
-    pg_engine = sa.create_engine("postgresql+psycopg2://robb:foob@127.0.0.1/pgdb")
+    pg_engine = sa.create_engine("postgresql+psycopg2://postgres:postgres@127.0.0.1/postgres")
     chunks = pd.read_sql("SELECT * FROM users", "sqlite:///fakedata.db", chunksize=10_000)
 
     start = time.time()
-    with pg_engine.connect() as pg_conn:
+    with pg_engine.connect() as pg_conn:  # type: ignore
         pg_conn.execute("DROP TABLE IF EXISTS  users")
         for df in chunks:
             df.to_sql("users", pg_conn, if_exists="append")
