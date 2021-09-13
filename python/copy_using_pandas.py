0f74ce7e55d4d3bf45357cbb46797f45866cd685
Robert Enzmann <robert.enzmann@antuit.com>
2021-09-12 21:34:43 -0400

    correct some minor errors in the python version

--- python/copy_using_pandas.py
+++ python/copy_using_pandas.py
@@ -4,9 +4,16 @@ import pandas as pd
 import sqlalchemy as sa
 
 
-if __name__ == '__main__':
-    pg_engine = sa.create_engine("postgresql+psycopg2://postgres:postgres@127.0.0.1/postgres")
-    chunks = pd.read_sql("SELECT * FROM users", "sqlite:///fakedata.db", chunksize=10_000)
+if __name__ == "__main__":
+    pg_engine = sa.create_engine(
+        "postgresql+psycopg2://postgres:postgres@127.0.0.1/postgres"
+    )
+    chunks = pd.read_sql(
+        "SELECT * FROM users",
+        "sqlite:///fakedata.db",
+        chunksize=10_000,
+        index_col="user_id",
+    )
 
     start = time.time()
     with pg_engine.connect() as pg_conn:  # type: ignore
